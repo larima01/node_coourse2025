69c58e61c6e37c15579553b9d3ee2e59d2f243f3
larim01 <lukasaminu@gmail.com>
2025-03-17 01:39:10 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+import getPosts, {getPostLenght} from "./postController.js";
+
+console.log(getPosts());
+console.log(`Post length: ${getPostLenght()}`)
+// const {generateRandomNumber, celciusToFahrenheit} = require("./utilis");
+
+// console.log(`generate number: ${generateRandomNumber()}`);
+// console.log(`Conversion : ${celciusToFahrenheit(35)}`)
