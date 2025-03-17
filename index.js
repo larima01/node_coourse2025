f2cf5b633a9685e31c97504e8a98d48d361d4630
larim01 <lukasaminu@gmail.com>
2025-03-17 17:11:57 +0100

    update

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ import getPosts, {getPostLenght} from "./postController.js";
 
 console.log(getPosts());
 console.log(`Post length: ${getPostLenght()}`)
+console.log(`Post: ${getPostLenght()}`)
 // const {generateRandomNumber, celciusToFahrenheit} = require("./utilis");
 
 // console.log(`generate number: ${generateRandomNumber()}`);
+// console.log(`Conversion : ${celciusToFahrenheit(35)}`)
 // console.log(`Conversion : ${celciusToFahrenheit(35)}`)
