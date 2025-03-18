a09409ccc3d8a1c7d9a4336e9dbac0455969401d
larim01 <lukasaminu@gmail.com>
2025-03-18 11:18:15 +0100

    updated

--- server.js
+++ server.js
@@ -1,11 +1,39 @@
+import { error } from 'console';
 import http from 'http';
-const PORT = 8000;
-const server = http.createServer((req, res) => {
-    // res.setHeader('Content-Type', 'text/html');
-    // res.statusCode = 404;
+import fs from 'fs/promises';
+import url from 'url';
+import path from 'path';
+// GET Current Path
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+console.log(__filename, __dirname)
+const PORT = process.env.PORT;
+const server = http.createServer(async (req, res) => {
+    try{
+        //Check if GET request
+        if(req.method === 'GET'){
+            let filePath;
+            if(req.url=== '/')
+                {
+                 filePath = path.join(__dirname, 'public', 'index.html');
+                } else if (req.url === '/about') {
+                    filePath = path.join(__dirname, 'public', 'about.html');
+                } else {
+                 throw new Error('Page not found');
+                }
 
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('<h1> Hello world</h1>');
+                const data = await fs.readFile(filePath);
+                res.setHeader('Content-Type', 'text/html');
+                res.write(data);
+                res.end();
+
+        }else {
+            throw new Error('Request not allowed');
+        }
+    } catch (error){
+        res.writeHead(500, {'Content-Type': 'text/plain'});
+                 res.end('Server error');
+    }   
 });
 
 server.listen(PORT, () => {
