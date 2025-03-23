c2275fa4a40e9a44a1292d5d8c1208346dc49f80
larim01 <lukasaminu@gmail.com>
2025-03-23 01:02:14 +0100

    Initial commit

--- cryptoDemo.js
+++ cryptoDemo.js
@@ -9,3 +9,13 @@ crypto.randomBytes(16, (err, buf) => {
     if (err) throw err;
     console.log(buf.toString('hex'));
 });
+
+// creteCipheriv & createDecipheriv
+const algorithm = 'aes-256-cbc';
+const key = crypto.randomBytes(32);
+const iv = crypto.randomBytes(16);
+
+const cipher = crypto.createCipheriv(algorithm, key, iv);
+let encryted = cipher.update('Hello, this is a secret message', 'utf8', 'hex');
+encryted += cipher.final('hex');
+console.log(encryted);
